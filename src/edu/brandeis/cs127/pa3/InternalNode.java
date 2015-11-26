b4e87ae3a9b1b89e9259abeaf012f3f6d209d331
SHU CHEN <chenshu8805@gmail.com>
2015-11-26 04:24:35 -0500

    some other b+tree

---
+++ src/edu/brandeis/cs127/pa3/InternalNode.java
@@ -0,0 +1,280 @@
+package edu.brandeis.cs127.pa3;
+
+/**
+    Internal Nodes of B+-Trees.
+    @author cs127b
+ */
+public class InternalNode extends Node{
+
+	/**
+       Construct an InternalNode object and initialize it with the parameters.
+       @param d degree
+       @param p0 the pointer at the left of the key
+       @param k1 the key value
+       @param p1 the pointer at the right of the key
+       @param n the next node
+       @param p the previous node
+	 */
+	public InternalNode (int d, Node p0, int k1, Node p1, Node n, Node p){
+
+		super (d, n, p);
+		ptrs [0] = p0;
+		keys [1] = k1;
+		ptrs [1] = p1;
+		lastindex = 1;
+
+		if (p0 != null) p0.setParent (new Reference (this, 0, false));
+		if (p1 != null) p1.setParent (new Reference (this, 1, false));
+	}
+
+	/**
+       The minimal number of keys this node should have.
+       @return the minimal number of keys a leaf node should have.
+	 */
+	public int minkeys () {
+		int min = 0;
+
+		///////////////////
+		// ADD CODE HERE //
+		///////////////////
+		
+		if (parentref==null) {
+			min = 1;
+		} else {
+			min = (degree-1)/2;
+		}
+
+		return min;
+	}
+
+	/**
+       Check if this node can be combined with other into a new node without splitting.
+       Return TRUE if this node and other can be combined. 
+	 */
+	public boolean combinable (Node other) {
+
+		boolean combinable = true;
+
+		///////////////////
+		// ADD CODE HERE //
+		///////////////////
+
+		if (lastindex+other.lastindex>=degree-1) combinable=false;
+		return combinable;
+	}
+
+
+	/**
+       Combines contents of this node and its next sibling (next)
+       into a single node,
+	 */
+	public void combine () {
+
+		///////////////////
+		// ADD CODE HERE //
+		///////////////////
+		
+		Node ns=this.next;
+		System.arraycopy(ns.keys, 1, keys, lastindex+1, ns.lastindex);
+		System.arraycopy(ns.ptrs, 1, ptrs, lastindex+1, ns.lastindex);
+		lastindex+=ns.lastindex;
+		this.setNext(ns.next);
+		if (this.next!=null) this.getNext().setPrev(this);
+	}
+
+	/**
+       Redistributes keys and pointers in this node and its
+       next sibling so that they have the same number of keys
+       and pointers, or so that this node has one more key and
+       one more pointer.  Returns the key that must be inserted
+       into parent node.
+       @return the value to be inserted to the parent node
+	 */
+	public int redistribute () {
+		
+		///////////////////
+		// ADD CODE HERE //  
+		///////////////////
+
+		Node ns = this.getNext();
+		int newLastindex = (lastindex+ns.lastindex)/2;
+		int keysShifted = Math.abs(lastindex-newLastindex);
+		
+		if (newLastindex<lastindex) { 
+			//redistribute keys
+			System.arraycopy(ns.keys, 1, ns.keys, keysShifted, ns.lastindex);
+			System.arraycopy(keys, newLastindex+1, ns.keys, 0, keysShifted);
+			//redistribute pointers
+			System.arraycopy(ns.ptrs, 0, ns.ptrs, keysShifted-1, ns.lastindex+1);
+			System.arraycopy(ptrs, newLastindex+1, ns.ptrs, 0, keysShifted);
+		} else {
+			//redistribute keys
+			System.arraycopy(ns.keys, 1, keys, lastindex+1, keysShifted);
+			System.arraycopy(ns.keys, keysShifted+1, ns.keys, 0, ns.lastindex-keysShifted);
+			//redistribute pointers
+			System.arraycopy(ns.ptrs, 0, ptrs, lastindex, keysShifted+1);
+			System.arraycopy(ns.ptrs, keysShifted+1, ns.ptrs, 0, ns.lastindex-keysShifted);
+		}
+		
+		ns.lastindex += lastindex-newLastindex-1;
+		if (ns.lastindex == 0) ns.lastindex++;
+		this.lastindex = newLastindex;		
+		
+		int toParent = ns.keys[0];
+		ns.keys[0]=0;
+		return toParent;
+	}
+	
+	/**
+       Inserts (val, ptr) pair into this node
+       at keys [i] and ptrs [i].  Called when this
+       node is not full.  Differs from {@link LeafNode} routine in
+       that updates parent references of all ptrs from index i+1 on.
+       @param val the value to insert
+       @param ptr the pointer to insert 
+       @param i the position to insert the value and pointer
+	 */
+	public void insertSimple (int val, Node ptr, int i) {
+
+		////////////////////
+		// ADD CODE HERE  //
+		////////////////////
+		
+		System.arraycopy(keys, i, keys, i+1, lastindex+1-i);
+		System.arraycopy(ptrs, i, ptrs, i+1, lastindex+1-i);
+		keys[i]=val;
+		ptrs[i]=ptr;
+		lastindex++;	
+	}
+
+	/**
+       Deletes keys [i] and ptrs [i] from this node,
+       without performing any combination or redistribution afterwards.
+       Does so by shifting all keys and pointers from index i+1 on
+       one position to the left.  Differs from {@link LeafNode} routine in
+       that updates parent references of all ptrs from index i+1 on.
+       @param i the index of the key to delete
+	 */
+	public void deleteSimple (int i) {
+
+		///////////////////
+		// ADD CODE HERE //
+		///////////////////
+		
+		if (i < lastindex) {
+			System.arraycopy(keys, i+1, keys, i, lastindex-i);
+			System.arraycopy(ptrs, i+1, ptrs, i, lastindex-i);	
+		}
+		lastindex--;	
+		readopt();
+	}
+
+
+	/**
+       Uses findPtrInex and calls itself recursively until find the value or find the position 
+       where the value should be.
+       @return the reference pointing to a leaf node.
+	 */
+	public Reference search (int val) {
+		Reference ref = null;
+
+		///////////////////
+		// ADD CODE HERE //
+		///////////////////
+		
+		int loc = this.findPtrIndex(val);
+		Node nextPtr = ptrs[loc];
+		ref = nextPtr.search(val);
+		return ref;
+	}
+
+	/**
+       Insert (val, ptr) into this node. Uses insertSimple, redistribute etc.
+       Insert into parent recursively if necessary
+       @param val the value to insert
+       @param ptr the pointer to insert 
+	 */
+	public void insert (int val, Node ptr) {
+
+		///////////////////
+		// ADD CODE HERE //
+		///////////////////		
+		
+//		if (val==146) {
+//			System.out.println(146);
+//		}
+//		
+		int toIndex = findKeyIndex(val);
+		
+		// if not full then just insert the key
+		if (!full() && keys[Math.min(toIndex,lastindex)]!=val) {
+			insertSimple(val,ptr,toIndex);
+			readopt();
+			return;
+		}
+		
+		// otherwise make a new right sibling for the current node, redistribute.
+		Node ns = null;
+		if (toIndex>lastindex) {
+			ns = new InternalNode(degree,null,val,ptr,next,(Node) this);
+		} else {
+			ns = new InternalNode(degree, null,keys[lastindex], ptrs[lastindex], next,(Node) this);
+			lastindex--;
+			insertSimple(val,ptr,toIndex);
+		}
+		
+		int toParent = redistribute();
+		// recursively insert into parent if exists
+		if (getParent()!=null) {
+			ns.setParent(parentref);
+			parentref.getNode().insert(toParent, ns);				
+		} else {
+			new InternalNode(degree,this,toParent,ns,null,null);
+		}
+		
+		this.readopt();
+		ns.readopt();
+	}
+
+	public void outputForGraphviz() {
+
+		// The name of a node will be its first key value
+		// String name = "I" + String.valueOf(keys[1]);
+		// name = BTree.nextNodeName();
+
+		// Now, prepare the label string
+		String label = "";
+		for (int j = 0; j <= lastindex; j++) {
+			if (j > 0) label += "|";
+			label += "<p" + ptrs[j].myname + ">";
+			if (j != lastindex) label += "|" + String.valueOf(keys[j+1]);
+			// Write out any link now
+			BTree.writeOut(myname + ":p" + ptrs[j].myname + " -> " + ptrs[j].myname + "\n");
+			// Tell your child to output itself
+			ptrs[j].outputForGraphviz();
+		}
+		// Write out this node
+		BTree.writeOut(myname + " [shape=record, label=\"" + label + "\"];\n");
+	}
+
+	/**
+       Print out the content of this node
+	 */
+	void printNode () {
+
+		int j;
+		System.out.print("[");
+		for (j = 0; j <= lastindex; j++) {
+
+			if (j == 0)
+				System.out.print (" * ");
+			else
+				System.out.print(keys[j] + " * ");
+
+			if (j == lastindex)
+				System.out.print ("]");
+		}
+	}
+}
+
+
---
+++ src/edu/brandeis/cs127/pa3/LeafNode.java
@@ -0,0 +1,239 @@
+package edu.brandeis.cs127.pa3;
+
+import java.util.Arrays;
+
+/**
+   LeafNodes of B+ trees
+ */
+public class LeafNode extends Node {
+
+	/**
+       Construct a LeafNode object and initialize it with the parameters.
+       @param d the degree of the leafnode
+       @param k the first key value of the node
+       @param n the next node 
+       @param p the previous node
+	 */
+	public LeafNode (int d, int k, Node n, Node p){
+		super (d, n, p);
+		keys [1] = k;
+		lastindex = 1;
+	}      
+
+
+	public void outputForGraphviz() {
+
+		// The name of a node will be its first key value
+		// String name = "L" + String.valueOf(keys[1]);
+		// name = BTree.nextNodeName();
+
+		// Now, prepare the label string
+		String label = "";
+		for (int j = 0; j < lastindex; j++) {
+			if (j > 0) label += "|";
+			label += String.valueOf(keys[j+1]);
+		}
+		// Write out this node
+		BTree.writeOut(myname + " [shape=record, label=\"" + label + "\"];\n");
+	}
+
+	/** 
+	the minimum number of keys the leaf node should have.
+	 */
+	public int minkeys () {
+		int min = 0;
+
+		///////////////////
+		// ADD CODE HERE //
+		///////////////////	
+		
+		if (parentref==null) {
+			min = 1;
+		} else {
+			min = degree/2;
+		}
+		return min;
+	}
+
+	/**
+       Check if this node can be combined with other into a new node without splitting.
+       Return TRUE if this node and other can be combined. 
+       @return true if this node can be combined with other; otherwise false.
+	 */
+	public boolean combinable (Node other){
+		
+		///////////////////
+		// ADD CODE HERE //
+		///////////////////
+		
+		if (lastindex+other.lastindex>=degree) return false;
+		return true;
+	}
+
+	/**
+       Combines contents of this node and its next sibling (nextsib)
+       into a single node
+	 */
+	public void combine (){
+
+		///////////////////
+		// ADD CODE HERE //
+		///////////////////
+		
+		Node ns=this.next;
+		System.arraycopy(ns.keys, 1, keys, lastindex+1, ns.lastindex);
+		lastindex+=ns.lastindex;
+		this.next = ns.next;
+		if (this.next!=null) this.next.prev = this;
+	}
+
+	/**
+       Redistributes keys and pointers in this node and its
+       next sibling so that they have the same number of keys
+       and pointers, or so that this node has one more key and
+       one more pointer,.  
+       @return int Returns key that must be inserted
+       into parent node.
+	 */
+	public int redistribute (){  
+
+		///////////////////
+		// ADD CODE HERE //
+		///////////////////
+
+		Node ns = this.getNext();
+		int newLastindex = (lastindex+ns.lastindex+1)/2;
+		int keysShifted = Math.abs(lastindex-newLastindex);
+		
+		if (newLastindex<lastindex) {
+			System.arraycopy(ns.keys, 1, ns.keys, keysShifted+1, ns.lastindex);
+			System.arraycopy(keys, newLastindex+1, ns.keys, 1, keysShifted);
+		} else {
+			System.arraycopy(ns.keys, 1, keys, lastindex+1, keysShifted);
+			System.arraycopy(ns.keys, 1+keysShifted, ns.keys, 1, ns.lastindex-keysShifted);
+		}
+		ns.lastindex += lastindex-newLastindex;
+		lastindex = newLastindex;
+		return ns.keys[1];
+	}
+
+	/**
+       Insert val into this node at keys [i].  (Ignores ptr) Called when this
+       node is not full.
+       @param val the value to insert to current node
+       @param ptr not used now, use null when call this method 
+       @param i the index where this value should be
+	 */
+	public void insertSimple (int val, Node ptr, int i){
+
+		///////////////////
+		// ADD CODE HERE //
+		///////////////////
+		
+		System.arraycopy(keys, i, keys, i+1, lastindex+1-i);
+		keys[i] = val;
+		lastindex++;
+	}
+
+
+	/**
+       Deletes keys [i] and ptrs [i] from this node,
+       without performing any combination or redistribution afterwards.
+       Does so by shifting all keys from index i+1 on
+       one position to the left.  
+	 */
+	public void deleteSimple (int i){
+
+		///////////////////
+		// ADD CODE HERE //
+		///////////////////	
+		
+		System.arraycopy(keys, i+1, keys, i, lastindex-i);
+		lastindex--;
+	} 
+
+	/**
+       Uses findKeyIndex, and if val is found, returns the reference with match set to true, otherwise returns
+       the reference with match set to false.
+       @return a Reference object referring to this node. 
+	 */
+	public Reference search (int val){
+
+		///////////////////
+		// ADD CODE HERE //
+		///////////////////
+
+		int loc = this.findKeyIndex(val);
+		boolean foundYa = false;
+		if (loc>lastindex) {
+			foundYa = val==keys[loc-1];
+			loc=lastindex;
+		} else {
+			foundYa = val==keys[loc];
+		}
+		Reference ref = new Reference(this, loc, foundYa);
+		return ref;
+	}
+
+	/**
+       Insert val into this, creating split
+       and recursive insert into parent if necessary
+       Note that ptr is ignored.
+       @param val the value to insert
+       @param ptr (not used now, use null when calling this method)
+	 */
+	public void insert (int val, Node ptr){
+
+		///////////////////
+		// ADD CODE HERE //
+		///////////////////
+				
+		if (val==145) {
+			System.out.println(145);
+		}
+		
+		int toIndex = findKeyIndex(val);
+		
+		// if value already exists, abort. Useless??????
+		if (search(val).getMatch()) {
+			return;
+		}
+		
+		// if leafnode not full then just insert the key
+		if (!full()) {
+			insertSimple(val,null,toIndex);
+			return;
+		}
+		
+		// otherwise make a new right sibling for the current node, redistribute.
+		Node ns = null;
+		if (toIndex>lastindex) {
+			ns = new LeafNode(degree, val, this.next,(Node) this);
+		} else {
+			ns = new LeafNode(degree, keys[lastindex], this.next,(Node) this);
+			lastindex--;
+			insertSimple(val,null,toIndex);
+		}
+	
+		int toParent = redistribute();
+		
+		//insert into parent
+		if (this.parentref!=null) {
+			ns.setParent(this.parentref);
+			this.getParent().getNode().insert(toParent, ns);
+		} else {
+			new InternalNode(degree,this,toParent,ns,null,null); 
+		}
+	}
+
+
+	/**
+       Print to stdout the content of this node
+	 */
+	void printNode (){
+		System.out.print ("[");
+		for (int i = 1; i < lastindex; i++) 
+			System.out.print (keys[i]+" ");
+		System.out.print (keys[lastindex] + "]");
+	}
+}
